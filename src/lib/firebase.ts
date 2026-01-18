90d1828cb831e7d92f2aafb88dbfd4d3651e8d1f
Nayan <199917572+KumarNayan11@users.noreply.github.com>
2026-01-18 16:36:02 +0530

    chore: move firebase config to environment variables
    Secured Firebase configuration using environment variables
    to keep public repository safe.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDd4uO7yCC1M3-NmTFpA8nDioGoc7xzLKQ",
-  authDomain: "planner-app-180c5.firebaseapp.com",
-  projectId: "planner-app-180c5",
-  storageBucket: "planner-app-180c5.firebasestorage.app",
-  messagingSenderId: "704621282536",
-  appId: "1:704621282536:web:bb8e57556b8996ab25b55d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
